30d6bd464a1c50e63c04da56c0a2c292a8e2c537
zi <zi@163.com>
2022-08-10 09:27:58 +0800

    注册中心搭建及api-passenger测试

--- service-vertificationcode/src/main/java/com/zi/servicevertificationcode/ServiceVertificationcodeApplication.java
+++ service-vertificationcode/src/main/java/com/zi/servicevertificationcode/ServiceVertificationcodeApplication.java
@@ -1,4 +1,4 @@
-package com.zi.apipassenger;
+package com.zi.servicevertificationcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiPassengerApplication {
+public class ServiceVertificationcodeApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ApiPassengerApplication.class);
+        SpringApplication.run(ServiceVertificationcodeApplication.class, args);
     }
+
 }
---
+++ service-vertificationcode/src/test/java/com/zi/servicevertificationcode/ServiceVertificationcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zi.servicevertificationcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceVertificationcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
