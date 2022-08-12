32016f853e70630b9e75535c323a19253a6b142f
zi <zi@163.com>
2022-08-12 19:59:15 +0800

    使用jwt进行token的解析

--- internal-common/src/main/java/com/zi/internalcommon/util/JwtUtils.java
+++ internal-common/src/main/java/com/zi/internalcommon/util/JwtUtils.java
@@ -3,6 +3,8 @@ package com.zi.internalcommon.util;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -11,10 +13,14 @@ import java.util.Map;
 
 public class JwtUtils {
 
-    private static final String SIGN = "fjao%304208*@#)*";
+    private static final String SIGN = "fao%208*@#)*";
+
+    private static final String JWT_KEY = "passengerPhone";
 
     //生成token
-    public static String generatorToken(Map<String, String> map){
+    public static String generatorToken(String passengerPhone){
+        Map<String, String> map = new HashMap<>();
+        map.put(JWT_KEY, passengerPhone);
         //token的过期时间
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE, 1);
@@ -35,12 +41,18 @@ public class JwtUtils {
         return sign;
     }
 
+    //解析token
+    public static String parseToken(String token){
+        DecodedJWT verify = JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
+        Claim claim = verify.getClaim(JWT_KEY);
+        return claim.toString();
+    }
+
     //测试
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
-        map.put("name", "lisi");
-        map.put("age", "23");
-        String s = generatorToken(map);
+        String s = generatorToken("17372804728");
         System.out.println("生成的token：" + s);
+
+        System.out.println("解析后的token：" + parseToken(s));
     }
 }
